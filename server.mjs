430cf7affe786ea5e257ddbff058a48184c5c5c9
v15hv4 <vishva2912@gmail.com>
2023-01-21 17:54:55 +0530

    Update dependencies, remove file watcher for supergraph schema

--- server.mjs
+++ server.mjs
@@ -3,104 +3,70 @@ import { expressMiddleware } from "@apollo/server/express4";
 import { ApolloGateway, RemoteGraphQLDataSource } from "@apollo/gateway";
 import { ApolloServerPluginDrainHttpServer } from "@apollo/server/plugin/drainHttpServer";
 
-import { watch } from "fs";
-import { readFile } from "fs/promises";
-
-import express from "express";
-import { expressjwt } from "express-jwt";
-
 import http from "http";
 import cors from "cors";
+import express from "express";
 import bodyParser from "body-parser";
 
-// server config
+import { readFileSync } from "fs";
+import { expressjwt } from "express-jwt";
+
+// gateway config
 const port = process.env.GATEWAY_PORT || 80;
-const secret = process.env.JWT_SECRET || "this-is-the-greatest-secret-of-all-time";
+const jwt_secret = process.env.JWT_SECRET || "this-is-the-greatest-secret-of-all-time";
 const corsOptions = {
-  origin: (process.env.GATEWAY_ALLOWED_ORIGINS || "0.0.0.0 localhost 127.0.0.1").split(" "),
+  origin: (process.env.GATEWAY_ALLOWED_ORIGINS || "localhost 127.0.0.1").split(" "),
   credentials: true,
 };
 const supergraphSchema = "/data/supergraph.graphql";
 
+// instantiate express app
+const app = express();
+
+// httpServer handles incoming requests to the express app
+// enable graceful shutdown by telling Apollo Server to drain the httpServer
+const httpServer = http.createServer(app);
+
 // instantiate gateway
 const gateway = new ApolloGateway({
-  async supergraphSdl({ update, healthCheck }) {
-    const watcher = watch(supergraphSchema);
-
-    // reload supergraph on update
-    watcher.on("change", async () => {
-      try {
-        const updatedSupergraph = await readFile(supergraphSchema);
-        await healthCheck(updatedSupergraph);
-        update(updatedSupergraph);
-      } catch (e) {
-        console.error(e);
-      }
-    });
-
-    return {
-      supergraphSdl: await readFile(supergraphSchema, "utf-8"),
-      async cleanup() {
-        watcher.close();
-      },
-    };
-  },
-
-  buildService({ url }) {
-    return new RemoteGraphQLDataSource({
+  supergraphSdl: readFileSync(supergraphSchema).toString(),
+  buildService: ({ url }) =>
+    new RemoteGraphQLDataSource({
       url,
 
       // pass user as context item
-      willSendRequest({ request, context }) {
-        request.http.headers.set(
-          "user",
-          context.user ? JSON.stringify(context.user) : null
-        );
+      willSendRequest: ({ request, context }) => {
+        request.http.headers.set("user", context.user ? JSON.stringify(context.user) : null);
       },
-
-      // forward all headers from subgraphs
-      didReceiveResponse({ response, context }) {
-        const headers = response?.http?.headers;
-        headers?.forEach((value, key) => {
-          context?.res?.setHeader?.(key, value);
-        });
-        return response;
-      },
-    });
-  },
+    }),
 });
 
-// set up express server
-const app = express();
-
-// set up HTTP server
-const httpServer = http.createServer(app);
+// instantiate server
 const server = new ApolloServer({
   gateway: gateway,
-  plugins: [ApolloServerPluginDrainHttpServer({ httpServer })], // for graceful shutdown
+  plugins: [ApolloServerPluginDrainHttpServer({ httpServer })],
 });
 
-// wait for server to start
+// ensure we wait for server to start
 await server.start();
 
-// add middleware
+// set up middleware
 app.use(
   "/",
   cors(corsOptions),
   bodyParser.json(),
   expressjwt({
-    secret: secret,
+    secret: jwt_secret,
     algorithms: ["HS256"],
     credentialsRequired: false,
   }),
   expressMiddleware(server, {
-    context: ({ req, res }) => {
-      const user = req.auth || null;
-      return { user, req, res };
-    },
+    context: ({ req }) => ({
+      user: req.auth || null,
+    }),
   })
 );
 
 // modified server startup
-await new Promise((resolve) => httpServer.listen({ port: port }, resolve));
+await new Promise((resolve) => httpServer.listen({ port }, resolve));
 console.log(`Gateway started at port ${port}.`);
